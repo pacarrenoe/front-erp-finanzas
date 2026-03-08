c42a87e330ea9860e2be488160d35457051e86b6
pacarrenoe <carrenopatricia022@gmail.com>
2026-03-08 03:56:31 -0300

    Cambios

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -85,10 +85,6 @@ export default function Login() {
           Entrar
         </button>
 
-        <p className={styles.loginLink}>
-          ¿No tienes cuenta? <a href="/register">Regístrate</a>
-        </p>
-
       </form>
 
     </div>
